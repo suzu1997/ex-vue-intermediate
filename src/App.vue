206965b87759f8c5b2fc5a6714d49d7488aaf575
Suzuhara Chie <mignon.ange.3@gmail.com>
2021-11-11 12:09:50 +0900

    演習1のルーティングとリンクを設定

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/baseballTeamList">野球チーム一覧</router-link> |
     </div>
     <router-view />
   </div>
