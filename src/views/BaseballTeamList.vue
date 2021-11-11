18637446e5a0d4ad2cc6f29aba5e2f26bf9e8d9b
Suzuhara Chie <mignon.ange.3@gmail.com>
2021-11-11 12:32:25 +0900

    野球チーム一覧表示機能のクラスコンポーネントを作成

--- src/views/BaseballTeamList.vue
+++ src/views/BaseballTeamList.vue
@@ -1,13 +1,23 @@
 <template>
-  
+  <div></div>
 </template>
 
-<script>
-export default {
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import { Team } from "../types/team";
 
+/**
+ * 野球チーム一覧表示機能のクラスコンポーネントです.
+ */
+@Component
+export default class baseballTeamList extends Vue {
+  /**
+   * Vuexストア内の野球チーム一覧を取得し返す.
+   */
+  get getTeams(): Array<Team> {
+    return this.$store.getters.getTeams;
+  }
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
