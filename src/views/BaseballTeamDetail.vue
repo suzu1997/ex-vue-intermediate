cb6ce210f35a1234152eb3e0e5cf7846422fec8b
Suzuhara Chie <mignon.ange.3@gmail.com>
2021-11-11 14:23:38 +0900

    チーム詳細画面を表示するクラスコンポーネントを作成

--- src/views/BaseballTeamDetail.vue
+++ src/views/BaseballTeamDetail.vue
@@ -2,12 +2,26 @@
   <div></div>
 </template>
 
-<script>
-export default {
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import { Team } from "../types/team";
 
+/**
+ * チーム詳細画面を表示するクラスコンポーネントです.
+ */
+@Component
+export default class baseballTeabDetail extends Vue {
+  // 現在の詳細ページのチーム情報
+  private currentTeam!: Team;
+  /**
+   * リクエストパラメータのIDから1件のチーム情報を取得.
+   */
+  created(): void {
+    const id = parseInt(this.$route.params.id);
+
+    this.currentTeam = this.$store.getters.getTeamById(id);
+  }
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
